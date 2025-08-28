37d0dd69aa3fd094aa3179b530520772d2763ebb
Jianan Zhu <jianaz11@uci.edu>
2025-08-27 21:46:56 -0700

    Initial Shop UI Setup

---
+++ Brackeys Game Jam 2025/Assets/Scripts/ShopUI.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class ShopUI : MonoBehaviour
+{
+    [SerializeField] private GameObject _container;
+
+    [Header("Item Box One")]
+    [SerializeField] private GameObject _itemIconOne;
+    [SerializeField] private TextMeshProUGUI _nameDisplayerOne;
+    [SerializeField] private TextMeshProUGUI _biscuitCounterOne;
+
+    [Header("Item Box Two")]
+    [SerializeField] private GameObject _itemIconTwo;
+    [SerializeField] private TextMeshProUGUI _nameDisplayerTwo;
+    [SerializeField] private TextMeshProUGUI _biscuitCounterTwo;
+
+
+    [Header("Item Box Three")]
+    [SerializeField] private GameObject _itemIconThree;
+    [SerializeField] private TextMeshProUGUI _nameDisplayerThree;
+    [SerializeField] private TextMeshProUGUI _biscuitCounterThree;
+    
+
+ 
+}
